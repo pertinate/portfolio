389e586b38021220a1a93b3b4a207831be6840a2
Pertinate <ppertinate@gmail.com>
2023-11-19 15:11:28 -0600

    adding spacing to projects

--- src/components/windows/currentProjects.tsx
+++ src/components/windows/currentProjects.tsx
@@ -37,30 +37,36 @@ const CurrentProjects = () => {
         >
             <div className='flex gap-4 mx-2 flex-col'>
                 <InfoCard title='Portfolio'>
-                    <Text>{`The Personal Portfolio is a dynamic and interactive web application built using the powerful T3 stack, featuring Next.js, trpc, Mantine, Prisma, and Zustand. This platform serves as my dedicated digital playground, designed to showcase a diverse array of projects and provide a centralized space for individuals and organizations to connect with me regarding job opportunities.`}</Text>
-                    <Text>{`Utilizing the latest technologies, the portfolio offers a seamless and engaging user experience, allowing visitors to explore my projects, delve into their details, and gain insights into my skill set and capabilities. The inclusion of the T3 stack ensures robustness, scalability, and a modern development approach.`}</Text>
-                    <Text>{`As a dynamic hub for my professional endeavors, the portfolio emphasizes not only the projects themselves but also acts as a point of contact for potential job offers and collaborations. Its purpose is to reflect my passion for technology, innovation, and continual learning, making it a valuable resource for anyone interested in exploring my work and reaching out for potential collaborations or employment opportunities.`}</Text>
-                    <Button
-                        variant='light'
-                        component={Link}
-                        href={'https://github.com/pertinate/portfolio'}
-                        target='_blank'
-                    >
-                        View Code
-                    </Button>
+                    <div className='flex flex-col gap-4'>
+                        <Text>{`The Personal Portfolio is a dynamic and interactive web application built using the powerful T3 stack, featuring Next.js, trpc, Mantine, Prisma, and Zustand. This platform serves as my dedicated digital playground, designed to showcase a diverse array of projects and provide a centralized space for individuals and organizations to connect with me regarding job opportunities.`}</Text>
+                        <Text>{`Utilizing the latest technologies, the portfolio offers a seamless and engaging user experience, allowing visitors to explore my projects, delve into their details, and gain insights into my skill set and capabilities. The inclusion of the T3 stack ensures robustness, scalability, and a modern development approach.`}</Text>
+                        <Text>{`As a dynamic hub for my professional endeavors, the portfolio emphasizes not only the projects themselves but also acts as a point of contact for potential job offers and collaborations. Its purpose is to reflect my passion for technology, innovation, and continual learning, making it a valuable resource for anyone interested in exploring my work and reaching out for potential collaborations or employment opportunities.`}</Text>
+                        <Button
+                            variant='light'
+                            component={Link}
+                            href={'https://github.com/pertinate/portfolio'}
+                            target='_blank'
+                        >
+                            View Code
+                        </Button>
+                    </div>
                 </InfoCard>
                 <InfoCard title='Fullstack Template'>
-                    <Text>{`This project is a versatile template designed for efficient server-side rendering (SSR) using a stack that includes Mantine, React Router Dom, Fastify, Vite, Tailwind, Prisma, and Zod for packages and types. A key feature is the seamless sharing of types between the backend and frontend through the packages/types module, fostering consistency and reducing redundancy.`}</Text>
-                    <Text>{`This template serves as an ideal starting point for projects requiring SSR, offering a robust foundation for mocking server-side rendering and providing easy access to meta tags. The shared module, encompassing routes, backend types, and frontend types, ensures a unified development experience by maintaining a single source of truth for data structures.`}</Text>
-                    <Text>{`The integration of Zod for validation within the shared module enhances data integrity, offering a reliable mechanism for enforcing data constraints across both the frontend and backend. With a focus on modularity and maintainability, this SSR template empowers developers to accelerate their projects by providing a standardized structure and shared types for seamless communication between the server and client components.`}</Text>
-                    <Button
-                        variant='light'
-                        component={Link}
-                        href={'https://github.com/pertinate/fullstack-template'}
-                        target='_blank'
-                    >
-                        View Code
-                    </Button>
+                    <div className='flex flex-col gap-4'>
+                        <Text>{`This project is a versatile template designed for efficient server-side rendering (SSR) using a stack that includes Mantine, React Router Dom, Fastify, Vite, Tailwind, Prisma, and Zod for packages and types. A key feature is the seamless sharing of types between the backend and frontend through the packages/types module, fostering consistency and reducing redundancy.`}</Text>
+                        <Text>{`This template serves as an ideal starting point for projects requiring SSR, offering a robust foundation for mocking server-side rendering and providing easy access to meta tags. The shared module, encompassing routes, backend types, and frontend types, ensures a unified development experience by maintaining a single source of truth for data structures.`}</Text>
+                        <Text>{`The integration of Zod for validation within the shared module enhances data integrity, offering a reliable mechanism for enforcing data constraints across both the frontend and backend. With a focus on modularity and maintainability, this SSR template empowers developers to accelerate their projects by providing a standardized structure and shared types for seamless communication between the server and client components.`}</Text>
+                        <Button
+                            variant='light'
+                            component={Link}
+                            href={
+                                'https://github.com/pertinate/fullstack-template'
+                            }
+                            target='_blank'
+                        >
+                            View Code
+                        </Button>
+                    </div>
                 </InfoCard>
                 <InfoCard title='Median XL Trade Site'>
                     <div className='flex flex-col gap-4'>
